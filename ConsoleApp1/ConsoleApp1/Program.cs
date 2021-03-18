71a09ca80fa78fa80d03ca25469a5dd1c195a058
mtria <massimo.tria@gmail.com>
2021-03-18 17:03:48 +0100

    Update Program.cs

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // questo commento è stato aggiunto in GitHub
         }
     }
 }
