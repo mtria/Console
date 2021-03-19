231234b967fa191001e831d36b2b34e570916a93
Massimo Tria <massimo.tria@gmail.com>
2021-03-19 16:16:57 +0100

    Cambiato l'entry point dell'app

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -2,26 +2,44 @@
 using AsyncAwaitConsoleTest;
 using TcpServer;
 using BackGroundWorkerTest;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        async static void Main(string[] args)
+        async static Task Main(string[] args)
         {
             Console.WriteLine("Hello World!");
             // questo commento è stato editato in GitHub
             // questo commento è stato aggiunto in VS
 
             // Async Await feature
-            var lr = new LongRunning();
+            await ProvaAsyncAwait();
 
             // TcpServer feature (modificato anche metodo main)
-            var test = new TestTcpServer();
-            await test.Test2();
+            //var test = new TestTcpServer();
+            //await test.Test2();
 
             // qui verrà aggiunto il riferimento al nuovo progetto BackgroundWorker
-            new Worker().Run();
+            //new Worker().Run();
+        }
+
+        async static Task ProvaAsyncAwait()
+        {
+            string s1 = null;
+            var s2 = "stringa";
+            if (s1 == s2)
+            {
+                Console.WriteLine("Le stringhe sono uguali");
+            }
+            var lr = new LongRunning();
+            var stringa1 = await lr.GetString();
+            Console.WriteLine(stringa1);
+
+            Console.WriteLine("Stringa finale");
+
+            Console.ReadKey();
         }
 
         static int Add(int x1, int x2)
